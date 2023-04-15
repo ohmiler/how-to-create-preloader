151f579bb976551c6d6c77240cf6f284a9efe0dc
Patiphan Phengpao <misterpatipan123@gmail.com>
2023-04-15 16:27:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function activeShowPage() {
+    let content = setTimeout(showPage, 3000);
+}
+  
+function showPage() {
+    document.getElementById("preLoader").style.display = "none";
+    document.getElementById("pageContent").style.display = "block";
+}
